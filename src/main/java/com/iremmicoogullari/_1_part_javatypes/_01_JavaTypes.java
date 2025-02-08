9371c144fca5854ec23ee6fb1baa59a98b4216f7
iremmicoogullari <irem.micoogullari@beat.solutions>
2025-02-08 18:29:33 +0300

    class, pvm, sout

--- src/main/java/com/iremmicoogullari/_1_part_javatypes/_01_JavaTypes.java
+++ src/main/java/com/iremmicoogullari/_1_part_javatypes/_01_JavaTypes.java
@@ -1,5 +1,10 @@
 package com.iremmicoogullari._1_part_javatypes;
 
 public class _01_JavaTypes {
-
-}
+    // psvm + tab
+    public static void main(String[] args) {
+    // sout + tab
+    // çıktı
+        System.out.println("BSc İrem Miçooğulları ! ;)");
+    } // end psvm
+} // end class
