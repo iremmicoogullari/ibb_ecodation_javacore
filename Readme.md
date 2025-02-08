f7ae5af6b6e91b6a4c99e94d1ff831c800d0fb24
iremmicoogullari <irem.micoogullari@beat.solutions>
2025-02-08 18:40:45 +0300

    class, pvm, sout

--- Readme.md
+++ Readme.md
@@ -1,1 +1,10 @@
 # IBB ecodation java core
+
+#GIT
+./sh
+Single Comment   => Ctrl+/
+Multiple Comment => Shift+Ctrl+/
+Beautify: Ctrl+Alt+l
+Copyand pase: Ctrl+d
+Hepsini değiştir: Shift+F6
+./sh
--- src/main/java/com/iremmicoogullari/_1_part_javatypes/_01_JavaPsvm.java
+++ src/main/java/com/iremmicoogullari/_1_part_javatypes/_01_JavaPsvm.java
@@ -1,10 +1,10 @@
 package com.iremmicoogullari._1_part_javatypes;
 
-public class _01_JavaTypes {
+public class _01_JavaPsvm {
     // psvm + tab
     public static void main(String[] args) {
     // sout + tab
     // çıktı
-        System.out.println("BSc İrem Miçooğulları ! ;)");
+        System.out.println("BSc İrem Miçooğulları ! ;)))");
     } // end psvm
 } // end class
