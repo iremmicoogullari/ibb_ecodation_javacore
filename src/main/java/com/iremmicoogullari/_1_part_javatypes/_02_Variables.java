8f3ba961ea988ddd388c5acb338a1520a7ac2158
iremmicoogullari <irem.micoogullari@beat.solutions>
2025-02-09 23:22:56 +0300

    variables

--- src/main/java/com/iremmicoogullari/_1_part_javatypes/_02_Variables.java
+++ src/main/java/com/iremmicoogullari/_1_part_javatypes/_02_Variables.java
@@ -14,6 +14,9 @@ public class _02_Variables {
         */
        // veri isimlendirmeleri;
        //   1-) isim, sıfat veya zamir kullanmamız lazım.
+       //   Anlamlı ve açıklayıcı isimler seçilmeli.
+        int studentCounter = 18;
+        double hamBurgerxD = 355.6;
         int schoolNumber = 762;
         System.out.println(schoolNumber);
 
@@ -30,5 +33,9 @@ public class _02_Variables {
         // int schoolNumber = 762; // same variable name is not written!
         // int 44schooulNumber = 762; // sayıyla başlanılamaz
         // int ``schoolNumber = 762; // underscore ve ampersand dışındaki özel simgelerle başlanamaz.!
+
+        //değişmeyen değerler için örn: pi sayısı. Başına final gelir ve sonrasında tekrar o değişken ismi tanımlanamaz.
+        final int MAX_STUDENT = 555;
+        // MAX_STUDENT = 200; Tekrar tanımlanamaz. cannot assign a value to final variable MAX_STUDENT. HATASI ALINIR.
     }
 }
